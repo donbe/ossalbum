880830e206ec56b5fc971ae522e905f6a8bfd50c
donbe <donbe520@gmail.com>
2020-06-16 12:01:35 +0800

    init

---
+++ ossalbum/ossalbum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ossalbum
+//
+//  Created by donbe on 2020/6/16.
+//  Copyright © 2020 donbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ossalbum/ossalbum/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ossalbum
+//
+//  Created by donbe on 2020/6/16.
+//  Copyright © 2020 donbe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
